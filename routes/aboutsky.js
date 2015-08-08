f208fd9525d3c6425bba6e527e19b39c8ba9b81e
yunian <286571362@qq.com>
2015-08-08 14:49:13 +0800

    todos 1

--- routes/aboutsky.js
+++ routes/aboutsky.js
@@ -2,8 +2,9 @@
  * Created by Administrator on 2015-08-08.
  */
 var router = require('express').Router();
-var AV = require('leanengine');
 
 router.get('/', function (req,res,next){
-    res.render('todos');
+    res.render('todos',{title: 'TODO 列表', todos: 'yunian'});
 });
+
+module.exports = router;
